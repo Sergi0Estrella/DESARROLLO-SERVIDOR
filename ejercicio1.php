7e4493fda1792e9aa95eec376c69f68434bee5de
SerBio <72515238+Sergi0Estrella@users.noreply.github.com>
2022-10-25 12:20:46 +0200

    Add files via upload

---
+++ ejercicio1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre">
+        <label for="apellidos">Apellidos:</label>
+        <input type="text" name="apellidos" id="apellidos">
+        <br>
+        <label for="edad">Edad:</label>
+        <input type="text" name="edad" id="edad">
+        <label for="telefono">Teléfono</label>
+        <input type="text" name="telefono" id="telefono">
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+        <label for="foto">Foto</label>
+        <input type="file" name="foto" id="foto">
+        <br><br>
+        <input type="submit">
+        <input type="reset">
+    </form>
+</body>
+</html>
